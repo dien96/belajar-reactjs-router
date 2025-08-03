e01adddd566a1394faf384b5b96593c4e1754e89
dien96 <didinrudini12rplb@gmail.com>
2025-08-04 01:33:01 +0700

    Add routing and page components for Home, About, Data, Product, Costumer, and Seller

---
+++ src/Costumer.jsx
@@ -0,0 +1,8 @@
+export default function Costumer() {
+    return (
+        <div>
+            <h1>Costumer</h1>
+            <p>Welcome to the costumer page!</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './Home'
+import About from './About'
+import Product from './Product'
+import Costumer from './Costumer'
+import Seller from './Seller'
+import Data from './Data'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/data">
+          <Route index element={<Data />} />
+          <Route path="products" element={<Product />} />
+          <Route path="costumers" element={<Costumer />} />
+          <Route path="sellers" element={<Seller />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
