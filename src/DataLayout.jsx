75bb32b7759ee954d15939e118297eee7964892c
dien96 <didinrudini12rplb@gmail.com>
2025-08-04 08:01:34 +0700

    Add navigation links to DataLayout and implement button to navigate to data page in Home

--- src/DataLayout.jsx
+++ src/DataLayout.jsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
+import './data.css';
 
 export default function DataLayout() {
     return (
         <>
             <header>
                 <h1>This is header</h1>
+                <nav>
+                    <ul>
+                        <li><NavLink to="/data/products">Products</NavLink></li>
+                        <li><NavLink to="/data/sellers">Sellers</NavLink></li>
+                        <li><NavLink to="/data/customers">Customers</NavLink></li>
+                        <li><NavLink to={{
+                            pathname: "/data/products",
+                            search: "?category=shoes",
+                            hash: "#top"
+                        }}>Products</NavLink></li>
+                    </ul>
+                </nav>
             </header>
             <main>
                 <Outlet />
